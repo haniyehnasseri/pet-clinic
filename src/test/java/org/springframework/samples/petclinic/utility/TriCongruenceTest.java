40bba99bb8658ff172e7586366170635b33a9732
naseri <naseri@mahsan.co>
2021-11-25 11:09:16 +0330

    Q1 added

--- src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
@@ -9,14 +9,152 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ExtendWith(ReportingExtension.class)
+@ClauseDefinition(clause = 'a',def = "t1arr[0] != t2arr[0]")
+@ClauseDefinition(clause = 'b',def = "t1arr[1] != t2arr[1]")
+@ClauseDefinition(clause = 'c',def = "t1arr[2] != t2arr[2]")
+@ClauseDefinition(clause = 'd',def = "t1arr[0] < 0")
+@ClauseDefinition(clause = 'e',def = "t1arr[0] + t1arr[1] < t1arr[2]")
 class TriCongruenceTest {
 
 	private static final Logger log = LoggerFactory.getLogger(TriCongruenceTest.class);
 
+	// CUTPNF - FFF
+	@NearFalsePoint(
+		predicate = "a + b + c",
+		dnf = "a + b + c",
+		implicant = "c",
+		clause = 'c',
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
 	@Test
-	public void sampleTest() {
+	public void sampleTestCUTPNF1() {
+		Triangle t1 = new Triangle(7, 3, 3);
+		Triangle t2 = new Triangle(7, 3, 3);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+
+
+	// CUTPNF - TFF
+	@UniqueTruePoint(
+		predicate = "a + b +c",
+		dnf = "a + b + c",
+		implicant = "a",
+		valuations = {
+			@Valuation(clause = 'a', valuation = true),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@Test
+	public void sampleTestCUTPNF2() {
 		Triangle t1 = new Triangle(2, 3, 7);
+		Triangle t2 = new Triangle(7, 3, 7);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+
+	// CUTPNF - FTF
+	@UniqueTruePoint(
+		predicate = "a + b +c",
+		dnf = "a + b + c",
+		implicant = "b",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = true),
+			@Valuation(clause = 'c', valuation = false)
+		}
+	)
+	@Test
+	public void sampleTestCUTPNF3() {
+		Triangle t1 = new Triangle(7, 3, 3);
 		Triangle t2 = new Triangle(7, 2, 3);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+	// CUTPNF - FFT
+	@UniqueTruePoint(
+		predicate = "a + b +c",
+		dnf = "a + b + c",
+		implicant = "c",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false),
+			@Valuation(clause = 'c', valuation = true)
+		}
+	)
+	@Test
+	public void sampleTestCUTPNF4() {
+		Triangle t1 = new Triangle(7, 3, 7);
+		Triangle t2 = new Triangle(7, 3, 1);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+
+
+	// CACC - FF
+	@CACC(
+		predicate = "d + e",
+		majorClause = 'd',
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = false)
+		},
+		predicateValue = false
+	)
+	@Test
+	public void sampleTestCACC1() {
+		Triangle t1 = new Triangle(2, 3, 1);
+		Triangle t2 = new Triangle(2, 3, 1);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertTrue(areCongruent);
+	}
+
+	// CACC - FT
+	@CACC(
+		predicate = "d + e",
+		majorClause = 'e',
+		valuations = {
+			@Valuation(clause = 'd', valuation = false),
+			@Valuation(clause = 'e', valuation = true)
+		},
+		predicateValue = true
+	)
+	@Test
+	public void sampleTestCACC2() {
+		Triangle t1 = new Triangle(2, 3, 9);
+		Triangle t2 = new Triangle(2, 3, 9);
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
+		Assertions.assertFalse(areCongruent);
+	}
+
+
+	// CC - TT ( TF NOT POSSIBLE IN CACC -> CC added to cover d = true)
+	@ClauseCoverage(
+		predicate = "d + e",
+		valuations = {
+			@Valuation(clause = 'd', valuation = true),
+			@Valuation(clause = 'e', valuation = true)
+		}
+	)
+	@Test
+	public void sampleTestCC() {
+		Triangle t1 = new Triangle(-2, 3, 9);
+		Triangle t2 = new Triangle(-2, 3, 9);
 		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
 		log.debug("Triangles identified as '{}'.", areCongruent ? "Congruent" : "Not Congruent");
 		Assertions.assertFalse(areCongruent);
