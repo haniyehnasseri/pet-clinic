acf7dfd68fc580b81b3c9cdb571319eed331b197
naseri <naseri@mahsan.co>
2021-12-19 19:52:46 +0330

    ca6 - 95% mutation coverage

--- src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
@@ -36,7 +36,6 @@ class PriceCalculatorTest {
 		Pet pet3 = Mockito.spy(Pet.class);
 		Pet pet4 = Mockito.spy(Pet.class);
 		Pet pet5 = Mockito.spy(Pet.class);
-		//Pet pet6 = Mockito.spy(Pet.class);
 
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 2, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet1).getBirthDate();
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 2, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet2).getBirthDate();
@@ -54,7 +53,6 @@ class PriceCalculatorTest {
 		double total = PriceCalculator.calcPrice(Arrays.asList(pet1,pet2,pet3,pet4,pet5), 1000, 1000);
 
 		Assertions.assertEquals(32560.0, total);
-		System.out.println(total);
 
 	}
 
@@ -66,7 +64,6 @@ class PriceCalculatorTest {
 		Pet pet3 = Mockito.spy(Pet.class);
 		Pet pet4 = Mockito.spy(Pet.class);
 		Pet pet5 = Mockito.spy(Pet.class);
-		//Pet pet6 = Mockito.spy(Pet.class);
 
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 2, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet1).getBirthDate();
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 2, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet2).getBirthDate();
@@ -84,7 +81,6 @@ class PriceCalculatorTest {
 		double total = PriceCalculator.calcPrice(Arrays.asList(pet1,pet2,pet3,pet4,pet5), 1000, 1000);
 
 		Assertions.assertEquals(17120.0, total);
-		System.out.println(total);
 
 	}
 
@@ -96,7 +92,6 @@ class PriceCalculatorTest {
 		Pet pet3 = Mockito.spy(Pet.class);
 		Pet pet4 = Mockito.spy(Pet.class);
 		Pet pet5 = Mockito.spy(Pet.class);
-		//Pet pet6 = Mockito.spy(Pet.class);
 
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet1).getBirthDate();
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet2).getBirthDate();
@@ -114,7 +109,6 @@ class PriceCalculatorTest {
 		double total = PriceCalculator.calcPrice(Arrays.asList(pet1,pet2,pet3,pet4,pet5), 1000, 1000);
 
 		Assertions.assertEquals(17120.0, total);
-		System.out.println(total);
 
 	}
 
@@ -126,7 +120,6 @@ class PriceCalculatorTest {
 		Pet pet3 = Mockito.spy(Pet.class);
 		Pet pet4 = Mockito.spy(Pet.class);
 		Pet pet5 = Mockito.spy(Pet.class);
-		//Pet pet6 = Mockito.spy(Pet.class);
 
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet1).getBirthDate();
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet2).getBirthDate();
@@ -144,7 +137,6 @@ class PriceCalculatorTest {
 		double total = PriceCalculator.calcPrice(Arrays.asList(pet1,pet2,pet3,pet4,pet5), 1000, 1000);
 
 		Assertions.assertEquals(16120.0, total);
-		System.out.println(total);
 
 	}
 
@@ -156,7 +148,6 @@ class PriceCalculatorTest {
 		Pet pet3 = Mockito.spy(Pet.class);
 		Pet pet4 = Mockito.spy(Pet.class);
 		Pet pet5 = Mockito.spy(Pet.class);
-		//Pet pet6 = Mockito.spy(Pet.class);
 
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet1).getBirthDate();
 		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet2).getBirthDate();
@@ -174,41 +165,37 @@ class PriceCalculatorTest {
 		double total = PriceCalculator.calcPrice(Arrays.asList(pet1,pet2,pet3,pet4,pet5), 0, 0);
 
 		Assertions.assertEquals(0.0, total);
-		System.out.println(total);
+
+	}
+
+
+	@Test
+	void calcPriceTest_6() {
+		Pet pet1 = Mockito.spy(Pet.class);
+		Pet pet2 = Mockito.spy(Pet.class);
+		Pet pet3 = Mockito.spy(Pet.class);
+		Pet pet4 = Mockito.spy(Pet.class);
+		Pet pet5 = Mockito.spy(Pet.class);
+		Pet pet6 = Mockito.spy(Pet.class);
+
+		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet1).getBirthDate();
+		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet2).getBirthDate();
+		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet3).getBirthDate();
+		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet4).getBirthDate();
+		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet5).getBirthDate();
+		Mockito.doReturn(LocalDate.of(LocalDate.now().getYear() - 1, LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth())).when(pet6).getBirthDate();
+		Visit myVisit = Mockito.spy(Visit.class);
+		Mockito.doReturn(LocalDate.now().minusDays(100)).when(myVisit).getDate();
+		Mockito.doReturn(Collections.emptyList()).when(pet1).getVisitsUntilAge(1);
+		Mockito.doReturn(Collections.emptyList()).when(pet2).getVisitsUntilAge(1);
+		Mockito.doReturn(Collections.emptyList()).when(pet3).getVisitsUntilAge(1);
+		Mockito.doReturn(Collections.emptyList()).when(pet4).getVisitsUntilAge(1);
+		Mockito.doReturn(Collections.emptyList()).when(pet5).getVisitsUntilAge(1);
+		Mockito.doReturn(Collections.emptyList()).when(pet6).getVisitsUntilAge(1);
+
+		double total = PriceCalculator.calcPrice(Arrays.asList(pet1,pet2,pet3,pet4,pet5,pet6), 1000, 1000);
+
+		Assertions.assertEquals(34920.0, total);
 
 	}
 }
-
-//	public static double calcPrice(List<Pet> pets, double baseCharge, double basePricePerPet) {
-//		double totalPrice = 0;
-//		int discountCounter = 0;
-//		LocalDate today = LocalDate.now();
-//
-//		for (Pet pet : pets) {
-//			double price = 0;
-//			long age = YEARS.between(pet.getBirthDate(), today);
-//			long daysFromLastVisit = 0;
-//			List<Visit> visits = new ArrayList<>(pet.getVisitsUntilAge((int) age));
-//			if (visits.size() != 0)
-//				daysFromLastVisit = DAYS.between(visits.get(visits.size() - 1).getDate(), today);
-//
-//			price = basePricePerPet * BASE_RARE_COEF;
-//			if (age <= INFANT_YEARS) {
-//				price = price * RARE_INFANCY_COEF;
-//				discountCounter = discountCounter + 2;
-//			} else {
-//				discountCounter = discountCounter + 1;
-//			}
-//
-//			if (discountCounter >= DISCOUNT_MIN_SCORE) {
-//				if (daysFromLastVisit < 100) {
-//					totalPrice = (totalPrice * DISCOUNT_PRE_VISIT) + baseCharge;
-//				} else {
-//					totalPrice = (totalPrice + baseCharge) * (daysFromLastVisit / 100 + visits.size());
-//				}
-//			}
-//			totalPrice += price;
-//		}
-//
-//		return totalPrice;
-//	}
